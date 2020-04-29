6e506ef1a480ec4e219c9df680b50ca13b23f34d
Matthew Lamm <matthewjlamm@gmail.com>
2020-04-29 13:37:33 -0400

    Add User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Matthew', password: 'password')
+User.create(username: 'Anabel', password: 'password')
+User.create(username: 'Nick', password: 'password')
